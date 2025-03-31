a63de320bd429f3e9373cf76dd109b54c0f75fa9
carolinescranton01 <carolinescranton@arizona.edu>
2025-03-31 11:01:53 -0700

    uploading ex 19

---
+++ exercises/19_wod/wod.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+Author : Caroline Scranton <carolinescranton@arizona.edu>
+Date   : 2025-03-31
+Purpose: Create Workout Of (the) Day (WOD)
+"""
+
+import argparse
+import csv
+import io
+import random
+from tabulate import tabulate
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Create Workout Of (the) Day (WOD)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='CSV input file of exercises',
+                        metavar='str',
+                        type=argparse.FileType('rt'),
+                        default='inputs/exercises.csv')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Random seed',
+                        metavar='int',
+                        type=int,
+                        default=None)
+
+    parser.add_argument('-n',
+                        '--num',
+                        help='Number of exercises',
+                        metavar='int',
+                        type=int,
+                        default=4)
+
+    parser.add_argument('-e',
+                        '--easy',
+                        help='Half the reps',
+                        action='store_true',
+                        default=False)
+
+    args = parser.parse_args()
+
+    if args.num < 1:
+        parser.error(f'--num "{args.num}" must be greater than 0')
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    wod = []
+    exercises = read_csv(args.file)
+
+    for name, low, high in random.sample(exercises, k=args.num):
+        reps = random.randint(low, high)
+        if args.easy:
+            reps = int(reps / 2)
+        wod.append((name, reps))
+
+    print(tabulate(wod, headers=('Exercise', 'Reps')))
+
+
+# --------------------------------------------------
+def read_csv(fh):
+    """Read CSV"""
+
+    exercises = []
+    for row in csv.DictReader(fh, delimiter=','):
+        low, high = map(int, row['reps'].split('-'))
+        exercises.append((row['exercise'], low, high))
+
+    return exercises
+
+
+# --------------------------------------------------
+def test_read_csv():
+    """Test read_csv"""
+
+    text = io.StringIO('exercise,reps\nBurpees,20-50\nSitups,40-100')
+    assert read_csv(text) == [('Burpees', 20, 50), ('Situps', 40, 100)]
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
