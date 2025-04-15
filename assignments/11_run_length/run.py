fbcd0b2c5b8f4aeafba0c0365e3fc1d0cba4d4d5
carolinescranton01 <carolinescranton@arizona.edu>
2025-04-15 12:04:46 -0700

    hw 4-15-25

---
+++ assignments/11_run_length/run.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Author : Caroline Scranton <carolinescranton@arizona.edu>
+Date   : 2025-04-15
+Purpose: Run-length encoding
+"""
+
+import argparse
+import os
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Run-length encoding/data compression',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('sequence',
+                        metavar='str',
+                        help='DNA text or file')
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.sequence):
+        args.sequence = open(args.sequence).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    for seq in args.sequence.splitlines():
+        print(rle(seq))
+
+
+# --------------------------------------------------
+def rle(seq):
+    """ Create RLE """
+
+    rle_string = []
+    count = 1
+
+    for i in range(1, len(seq)):
+        if seq[i] == seq[i - 1]:
+            count += 1
+        else:
+            if count > 1:
+                rle_string.append(seq[i - 1] + str(count))
+            else:
+                rle_string.append(seq[i - 1])
+            count = 1
+
+    if count > 1:
+        rle_string.append(seq[-1] + str(count))
+    else:
+        rle_string.append(seq[-1])
+
+    return ''.join(rle_string)
+
+
+# --------------------------------------------------
+def test_rle():
+    """ Test rle """
+
+    assert rle('A') == 'A'
+    assert rle('ACGT') == 'ACGT'
+    assert rle('AA') == 'A2'
+    assert rle('AAAAA') == 'A5'
+    assert rle('ACCGGGTTTT') == 'AC2G3T4'
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
